fedecabc64d63b2b67fae9fee2bbcd69a46ea3a6
AdmiralFirefox <jjlcuadradev@gmail.com>
2022-12-28 22:23:12 +0800

    Added Card Line on Card Section

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -3,19 +3,25 @@ import styles from "../styles/cardsection/CardSection.module.scss";
 
 const CardSection = () => {
   return (
-    <div className={styles["cards-wrapper"]}>
-      <div className={styles["cards-container"]}>
-        {cardData.map((data) => (
-          <section key={data.id}>
-            <div className={styles["card-image-wrapper"]}>
-              <img src={data.imgPath} alt={data.cardTitle} />
-            </div>
-            <h1>{data.cardTitle}</h1>
-            <p>{data.cardDescription}</p>
-          </section>
-        ))}
+    <>
+      <div className={styles["card-line-wrapper"]}>
+        <div className={styles["card-line"]} />
       </div>
-    </div>
+
+      <div className={styles["cards-wrapper"]}>
+        <div className={styles["cards-container"]}>
+          {cardData.map((data) => (
+            <section key={data.id}>
+              <div className={styles["card-image-wrapper"]}>
+                <img src={data.imgPath} alt={data.cardTitle} />
+              </div>
+              <h1>{data.cardTitle}</h1>
+              <p>{data.cardDescription}</p>
+            </section>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
